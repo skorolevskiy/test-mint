a86fe3ecb7b1b09347010dec7934430621436071
Sergey Korolevskiy <s.korolevskiy@gmail.com>
2024-06-09 19:15:33 +0300

    new frame

--- src/app/api/frame/mint/route.ts
+++ src/app/api/frame/mint/route.ts
@@ -8,9 +8,11 @@ import {
   createPublicClient,
   createWalletClient,
   http,
-  formatUnits
+  formatUnits,
+  parseEther
 } from 'viem';
 import { privateKeyToAccount } from 'viem/accounts';
+import { getUser, updateRecieveDrop } from './../types';
 
 const NEYNAR_API_KEY = process.env.NEYNAR_API_KEY;
 const MINTER_PRIVATE_KEY = process.env.MINTER_PRIVATE_KEY as Hex | undefined;
@@ -43,75 +45,97 @@ export async function POST(req: NextRequest): Promise<Response> {
       throw new Error('Invalid frame request');
     }
 
-    // Check if user has an address connected
-    const address1: Address | undefined =
-        status?.action?.interactor?.verifications?.[0];
-    const address2: Address | undefined =
-        status?.action?.interactor?.verifications?.[1];
+    // // Check if user has an address connected
+    // const address1: Address | undefined =
+    //     status?.action?.interactor?.verifications?.[0];
+    // const address2: Address | undefined =
+    //     status?.action?.interactor?.verifications?.[1];
 
-    let rawBalance1: any, rawBalance2: any;
-    let balance1: bigint;
-    let balance2: bigint;
-    if (!address1) {
-        return getResponse(ResponseType.NO_ADDRESS);
-    } else {
-        // Check if user has a balance
-        rawBalance1 = await publicClient.readContract({
-            abi: abi,
-            address: CONTRACT_ADDRESS,
-            functionName: 'balanceOf',
-            args: [address1],
-            });
-        balance1 = BigInt(rawBalance1 as unknown as string);
-    }
-    if (!address2) {balance2 = BigInt(0);}
-    else {
-        rawBalance2 = await publicClient.readContract({
-            abi: abi,
-            address: CONTRACT_ADDRESS,
-            functionName: 'balanceOf',
-            args: [address2],
-            });
-        balance2 = BigInt(rawBalance2 as unknown as string)
-    }
+    // let rawBalance1: any, rawBalance2: any;
+    // let balance1: bigint;
+    // let balance2: bigint;
+    // if (!address1) {
+    //     return getResponse(ResponseType.NO_ADDRESS);
+    // } else {
+    //     // Check if user has a balance
+    //     rawBalance1 = await publicClient.readContract({
+    //         abi: abi,
+    //         address: CONTRACT_ADDRESS,
+    //         functionName: 'balanceOf',
+    //         args: [address1],
+    //         });
+    //     balance1 = BigInt(rawBalance1 as unknown as string);
+    // }
+    // if (!address2) {balance2 = BigInt(0);}
+    // else {
+    //     rawBalance2 = await publicClient.readContract({
+    //         abi: abi,
+    //         address: CONTRACT_ADDRESS,
+    //         functionName: 'balanceOf',
+    //         args: [address2],
+    //         });
+    //     balance2 = BigInt(rawBalance2 as unknown as string)
+    // }
 
-    const balanceInTokens1: number = parseInt(formatUnits(balance1, 18));
-    const balanceInTokens2: number = parseInt(formatUnits(balance2, 18));
-    const threshold: number = 24000;
+    // const balanceInTokens1: number = parseInt(formatUnits(balance1, 18));
+    // const balanceInTokens2: number = parseInt(formatUnits(balance2, 18));
+    // const threshold: number = 24000;
 	
-    if (balanceInTokens1 >= threshold || balanceInTokens2 >= threshold) {
-        console.warn(balanceInTokens1);
-        console.warn(balanceInTokens2);
-        // if (balanceInTokens1 >= threshold) {
-        // 	await updateWallet(fid_new, JSON.stringify(address1));
-        // } else if (balanceInTokens2 >= threshold) {
-        // 	await updateWallet(fid_new, JSON.stringify(address2));
-        // }
-    } else {
-        console.warn('1 need more token ' + balanceInTokens1 + ' - ' + address1);
-        console.warn('2 need more token ' + balanceInTokens2 + ' - ' + address2);
-        return getResponse(ResponseType.NEED_TOKEN);
-    }
+    // if (balanceInTokens1 >= threshold || balanceInTokens2 >= threshold) {
+    //     console.warn(balanceInTokens1);
+    //     console.warn(balanceInTokens2);
+    //     // if (balanceInTokens1 >= threshold) {
+    //     // 	await updateWallet(fid_new, JSON.stringify(address1));
+    //     // } else if (balanceInTokens2 >= threshold) {
+    //     // 	await updateWallet(fid_new, JSON.stringify(address2));
+    //     // }
+    // } else {
+    //     console.warn('1 need more token ' + balanceInTokens1 + ' - ' + address1);
+    //     console.warn('2 need more token ' + balanceInTokens2 + ' - ' + address2);
+    //     return getResponse(ResponseType.NEED_TOKEN);
+    // }
 
     const fid_new = status?.action?.interactor?.fid ? JSON.stringify(status.action.interactor.fid) : null;
     const power_badge = status?.action?.interactor?.power_badge ? status.action.interactor.power_badge : null;
 
     // Try minting a new token
-    const { request } = await publicClient.simulateContract({
-      address: CONTRACT_ADDRESS,
-      abi: abi,
-      functionName: 'transferTo',
-      args: [address1, 1n, '0x'],
-      account: privateKeyToAccount(MINTER_PRIVATE_KEY),
-    });
+    // const { request } = await publicClient.simulateContract({
+    //     account: privateKeyToAccount(MINTER_PRIVATE_KEY),
+    //   address: CONTRACT_ADDRESS,
+    //   abi: abi,
+    //   functionName: 'transferTo',
+    // });
+
+    const User = await getUser(fid_new);
+    let wallet;
+    if (!User) {
+        return getResponse(ResponseType.ERROR);
+    } else {
+        wallet = User.wallet;
+    }
+
+    let tokens: bigint = parseEther("100");
+
+    const request: any = await publicClient.prepareTransactionRequest({
+        account: privateKeyToAccount(MINTER_PRIVATE_KEY),
+        to: wallet,
+        value: tokens
+      })
+       
+      const serializedTransaction = await walletClient.signTransaction(request)
+       
+      
 
     if (!request) {
       throw new Error('Could not simulate contract');
     }
 
     try {
-      const hash = await walletClient.writeContract(request);
+        const hash = await walletClient.sendRawTransaction({ serializedTransaction });
 
+        if (hash) {
+            await updateRecieveDrop(fid_new, true);
+        }
     //   if (HAS_KV) {
     //     await kv.set(`mint:${address}`, hash);
     //   }
